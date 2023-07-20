313cc92849d5bae287639f595f566a9e0978a82f
plominskai <plominska.izabela@gmail.com>
2023-07-20 21:16:58 +0200

    Updated Main class with wedriver

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -12,11 +12,13 @@ import java.util.Set;
 public class Main {
     public static void main(String[] args) {
         System.setProperty("webdriver.chrome.driver", "C:\\Users\\plomi\\Projekt_koncowy_SDA_Guru99\\chromedriver\\chromedriver.exe");
-        ChromeOptions options = new ChromeOptions();
-        options.addArguments("--remote-allow-originals=*");
-        WebDriver driver = new ChromeDriver(options);
+
+        ChromeOptions ops = new ChromeOptions();
+        ops.addArguments("--remote-allow-origins=*");
+        WebDriver driver = new ChromeDriver(ops);
+
+
         driver.get("https://demo.guru99.com/v4/");
         driver.manage().window().maximize();
-        driver.findElement(By.xpath("/html/body/form/table/tbody/tr[1]/td[2]/input"));
     }
 }

d93520e8ac0751291c01df30f02537d4a7eadf61
plominskai <plominska.izabela@gmail.com>
2023-07-20 21:11:33 +0200

    Created Main class with wedriver

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,7 +1,22 @@
 package org.example;
 
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+
+import java.util.List;
+import java.util.Set;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        System.setProperty("webdriver.chrome.driver", "C:\\Users\\plomi\\Projekt_koncowy_SDA_Guru99\\chromedriver\\chromedriver.exe");
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("--remote-allow-originals=*");
+        WebDriver driver = new ChromeDriver(options);
+        driver.get("https://demo.guru99.com/v4/");
+        driver.manage().window().maximize();
+        driver.findElement(By.xpath("/html/body/form/table/tbody/tr[1]/td[2]/input"));
     }
 }

5357515a280c61582eed2a68889ab4299d80bdc0
plominskai <plominska.izabela@gmail.com>
2023-07-24 13:51:36 +0200

    HomePage added

---
+++ src/main/java/org/example/pages/HomePage.java
@@ -0,0 +1,27 @@
+package org.example.pages;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+
+public class HomePage {
+
+    WebDriver driver;
+
+    public HomePage(WebDriver driver) {
+        this.driver = driver;
+        PageFactory.initElements(driver, this);
+    }
+
+    @FindBy(xpath = "//*[@id=\"save\"]/span[1]/div/span")
+    WebElement acceptCookies;
+
+    public void navigateTo_HomePage() {
+        driver.get("https://demo.guru99.com/v4/");
+    }
+
+    public void clickAcceptCookies() {
+        acceptCookies.click();
+    }
+}
